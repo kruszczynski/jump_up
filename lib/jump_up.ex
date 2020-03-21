85f183249e6b7e3c35500cab3ff0870820ca9dee
Bartek Kruszczynski <kruszczyk@gmail.com>
2020-03-21 19:53:12 +0100

    initial commit

---
+++ lib/jump_up.ex
@@ -0,0 +1,18 @@
+defmodule JumpUp do
+  @moduledoc """
+  Documentation for JumpUp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JumpUp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jump_up_test.exs
@@ -0,0 +1,8 @@
+defmodule JumpUpTest do
+  use ExUnit.Case
+  doctest JumpUp
+
+  test "greets the world" do
+    assert JumpUp.hello() == :world
+  end
+end
