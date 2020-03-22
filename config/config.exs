4a179add4310b6ad9ea8d8e4059059e222623c10
Bartek Kruszczynski <kruszczyk@gmail.com>
2020-03-22 18:46:30 +0100

    Only sekrit on prod

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,4 @@ import Config
 config :jump_up, :megaboom, source_value: "foovalue"
 config :jump_up, :megaboom, target_mac: "00:11:22:33:44:55"
 
-import_config "secret.exs"
+if Mix.env() == :prod, do: import_config("secret.exs")
