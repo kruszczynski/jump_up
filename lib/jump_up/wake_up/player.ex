87325c5d08452895608f72a6a3c4696465af1d47
Bartek Kruszczynski <kruszczyk@gmail.com>
2020-03-23 23:00:34 +0100

    fix typos

--- lib/jump_up/wake_up/player.ex
+++ lib/jump_up/wake_up/player.ex
@@ -1,13 +1,16 @@
 defmodule JumpUp.WakeUp.Player do
   @music_dir Application.fetch_env!(:jump_up, :player)[:music_dir]
 
-  @player_exe "omxplater"
+  @player_exe "omxplayer"
   @default_options ["--vol", "-1200", "-o", "alsa:bluealsa"]
+
+  require Logger
 
   alias Porcelain.Process, as: Proc
 
   def start_playing do
     options = @default_options ++ [@music_dir]
+    Logger.info("Playing with options: #{options}")
 
     proc =
       Porcelain.spawn(@player_exe, options,
@@ -15,11 +18,16 @@ defmodule JumpUp.WakeUp.Player do
         out: {:path, "log/player.log"}
       )
 
+    Logger.info("Successfully started Playing")
+
     Process.sleep(20_000)
     Proc.send_input(proc, "+")
+    Logger.info("Volume turned up")
     Process.sleep(20_000)
     Proc.send_input(proc, "+")
+    Logger.info("Volume turned up")
     Process.sleep(20_000)
     Proc.send_input(proc, "+")
+    Logger.info("Volume turned up")
   end
 end
